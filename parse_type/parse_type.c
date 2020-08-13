6a2c8d9de1f1db9e23d87d0d5b557bc83567c237
rnakai <rnakai@student.42tokyo.jp>
2020-08-13 18:51:57 +0900

    output系関数内ではなく、parse_type内でfmtのポインタを進めるように修正

--- parse_type/parse_type.c
+++ parse_type/parse_type.c
@@ -6,32 +6,29 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 12:03:40 by rnakai            #+#    #+#             */
-/*   Updated: 2020/08/13 18:02:50 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/08/13 18:32:36 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-void				parse_type(t_flags *flags, char **fmt)
+void				parse_type(t_flags *flags, char **fmt, va_list argptr)
 {
 	if (**fmt == 'c')
-		output_c(flags, fmt);
+		output_c(flags, argptr);
 	else if (**fmt == 's')
-		output_s(flags, fmt);
+		output_s(flags, argptr);
 	else if (**fmt == 'p')
-		output_p(flags, fmt);
+		output_p(flags, argptr);
 	else if (**fmt == 'd' || **fmt == 'i')
-		output_di(flags, fmt);
+		output_di(flags, argptr);
 	else if (**fmt == 'u')
-		output_u(flags, fmt);
+		output_u(flags, argptr);
 	else if (**fmt == 'x')
-		output_x(flags, fmt);
+		output_x(flags, argptr);
 	else if (**fmt == 'X')
-		output_X(flags, fmt);
+		output_X(flags, argptr);
 	else if (**fmt == '%')
-		output_percent(flags, fmt);
-	else
-	{
-		(*fmt)++;
-	}
+		output_percent(flags, argptr);
+	(*fmt)++;
 }
