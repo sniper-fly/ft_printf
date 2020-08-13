b2469b8ceb62b5e73db5c8192dee61951471d337
rnakai <rnakai@student.42tokyo.jp>
2020-08-13 18:52:24 +0900

    c done

--- parse_type/output_c.c
+++ parse_type/output_c.c
@@ -1,12 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_c.c                                          :+:      :+:    :+:   */
+/*   output_c.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 12:04:53 by rnakai            #+#    #+#             */
-/*   Updated: 2020/08/13 12:04:54 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/08/13 18:46:51 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../ft_printf.h"
+#include <unistd.h>
+
+void				output_c(t_flags *flags, va_list argptr)
+{
+	int		actual_width;
+	char	ch;
+
+	ch = va_arg(argptr, char);
+	actual_width = MAX(1, flags->width);
+	flags->total_output_len += actual_width;
+	if (flags->minus_flag == 1)
+	{
+		write(1, &ch, 1);
+		put_it_xx_times(' ', actual_width - 1);
+	}
+	else if (flags->fill_zero_flag == 1)
+	{
+		put_it_xx_times('0', actual_width - 1);
+		write(1, &ch, 1);
+	}
+	else
+	{
+		put_it_xx_times(' ', actual_width - 1);
+		write(1, &ch, 1);
+	}
+}
