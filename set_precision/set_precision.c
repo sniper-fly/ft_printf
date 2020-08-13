8f99f18cfa6b2b0cb328aabe15a993666e4c9b0f
rnakai <rnakai@student.42tokyo.jp>
2020-08-13 17:46:07 +0900

    set_precision done but test not yet

--- set_precision/set_precision.c
+++ set_precision/set_precision.c
@@ -6,14 +6,23 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 12:04:10 by rnakai            #+#    #+#             */
-/*   Updated: 2020/08/13 16:50:05 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/08/13 17:45:19 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-void				set_precision_flag(t_flags *flags, char **fmt)
+void				set_precision(t_flags *flags, char **fmt)
 {
-	flags->precision_exsistence = 1;
-	(*fmt)++;
+	int		total;
+
+	while (**fmt == '0')
+		(*fmt)++;
+	total = 0;
+	while (ft_isdigit(**fmt))
+	{
+		total = (total * 10) + ((**fmt) - '0');
+		(*fmt)++;
+	}
+	flags->precision = total;
 }
--- set_precision/set_precision_flag.c
+++ set_precision/set_precision_flag.c
@@ -6,7 +6,14 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 12:02:52 by rnakai            #+#    #+#             */
-/*   Updated: 2020/08/13 12:02:53 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/08/13 17:45:22 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../ft_printf.h"
+
+void				set_precision_flag(t_flags *flags, char **fmt)
+{
+	flags->precision_exsistence = 1;
+	(*fmt)++;
+}
