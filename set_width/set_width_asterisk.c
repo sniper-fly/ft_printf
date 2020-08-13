9c7d86be9624d50a4b90f4388d0a7648b4caf5c9
rnakai <rnakai@student.42tokyo.jp>
2020-08-13 16:45:57 +0900

    set_width_asterisk finished but not yet tested

--- set_width/set_width_asterisk.c
+++ set_width/set_width_asterisk.c
@@ -6,7 +6,23 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 12:02:41 by rnakai            #+#    #+#             */
-/*   Updated: 2020/08/13 12:02:42 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/08/13 16:44:19 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../ft_printf.h"
+
+void				set_width_asterisk
+					(t_flags *flags, char **fmt, va_list argptr)
+{
+	int		tmp_width;
+
+	tmp_width = va_arg(argptr, int);
+	if (tmp_width < 0)
+	{
+		tmp_width *= -1;
+		flags->minus_flag = 1;
+	}
+	flags->width = tmp_width;
+	(*fmt)++;
+}
