c289c0a1da3c0c015b02de79c48601192d766fa6
rnakai <rnakai@student.42tokyo.jp>
2020-08-17 11:10:48 +0900

    zeroフラグが立っていた時の条件分岐がおかしかったので修正

--- parse_type/output_s.c
+++ parse_type/output_s.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 12:05:26 by rnakai            #+#    #+#             */
-/*   Updated: 2020/08/16 18:28:04 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/08/17 10:45:46 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static void			output_s2(t_flags *flags, int actual_strlen,
 		write(1, str, actual_strlen);
 		put_it_xx_times(' ', actual_width - actual_strlen);
 	}
-	else if (flags->minus_flag == 1)
+	else if (flags->fill_zero_flag == 1)
 	{
 		put_it_xx_times('0', actual_width - actual_strlen);
 		write(1, str, actual_strlen);
